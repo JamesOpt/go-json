101c39bf7c3c741c38bb093053cbd05e6f770676
hbs <hebingsong@linghit.com>
2022-04-11 23:11:14 +0800

    init

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"go-json/json"
 )
 
+type Person struct {
+	H int
+}
+
 type User struct {
 	Name,Address string
 	Age int `json:"gender"`
 	People []int `json:"haha"`
+	P []Person
 }
 
 func main()  {
@@ -17,7 +22,10 @@ func main()  {
 		Address: "wdwd",
 		Age:     0,
 		People:  []int{1, 2, 3},
+		P: []Person{{H:1}, {H:2}},
 	}
+
+	fmt.Println(u)
 	by, _ := json.Marshal(u)
 	fmt.Println(string(by))
 	ii := []int{1,2,3,4}
